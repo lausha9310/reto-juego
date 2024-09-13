941399ef5d001c459b4e2965ff6f8dbb80d4de11
Gabriel Guerrero <gagm2909@gmail.com>
2024-09-13 20:59:02 -0300

    asasdfgsfd

--- js/main.js
+++ js/main.js
@@ -1,93 +1,18 @@
-let usuarioScore = 0;
-let compuScore = 0;
-const usuarioScore_span = document.getElementById("usuario-score");
-const compuScore_span = document.getElementById("compu-score");
 
-const restart = document.getElementById("restart");
-const resultado = document.getElementById("resultado");
-const modal = document.querySelector(".modal");
-const piedra_div = document.getElementById("piedra");
-const papel_div = document.getElementById("papel");
-const tijeras_div = document.getElementById("tijeras");
+function startAnimation() {
+    const button = document.getElementById('flowerButton');
+    button.style.display = 'none'; // Oculta el botón al hacer clic
 
-function getCompuOpcione(){
-    const opciones = ['piedra', 'papel', 'tijeras'];
-    const randomNumber = Math.floor(Math.random() * 3);
-    return opciones[randomNumber];
+    const flowers = document.querySelectorAll('.flower');
+    flowers.forEach(flower => {
+        flower.style.animationPlayState = 'running'; // Inicia las animaciones
+    });
+
+    // Calcula el tiempo máximo de la animación más el retraso máximo
+    const maxAnimationTime = 10 * 1000 + 6 * 1000; // 10s de animación + 6s del mayor retraso
+
+    // Después de que termine la animación más larga, mostramos el botón
+    setTimeout(() => {
+        button.style.display = 'block'; // Muestra el botón de nuevo
+    }, maxAnimationTime);
 }
-
-function win(usuarioOpcione, compuOpcione){
-    usuarioScore++;
-    usuarioScore_span.innerHTML = usuarioScore;
-    compuScore_span.innerHTML = compuScore;
-    resultado.innerHTML = `<i class="icofont-close"></i><h1 class="text-win">Ganastes!</h1> <p>Compu eligio <strong>${compuOpcione}</strong></p>`;
-    modal.style.display = 'block';
-}
-
-function lose(usuarioOpcione, compuOpcione){
-    compuScore++;
-    usuarioScore_span.innerHTML = usuarioScore;
-    compuScore_span.innerHTML = compuScore;
-    resultado.innerHTML = `<i class="icofont-close"></i><h1 class="text-lose">Perdiste</h1> <p>Compu eligio <strong>${compuOpcione}</strong></p>`;
-    modal.style.display = 'block';
-}
-
-function draw(usuarioOpcione, compuOpcione){
-    usuarioScore_span.innerHTML = usuarioScore;
-    compuScore_span.innerHTML = compuScore;
-    resultado.innerHTML =  `<i class="icofont-close"></i><h1>Es un empate</h1> <p>Ambos eligieron <strong>${compuOpcione}</strong></p>`;
-    modal.style.display = 'block';
-}
-
-function play(usuarioOpcione){
-    const compuOpcione = getCompuOpcione();
-    switch (usuarioOpcione + compuOpcione) {
-        case 'piedratijeras':
-        case 'papelpiedra':
-        case 'tijeraspapel':
-            win(usuarioOpcione, compuOpcione);
-            break;
-        case 'piedrapapel':
-        case 'papeltijeras':
-        case 'tijeraspiedra':
-            lose(usuarioOpcione, compuOpcione);
-            break;
-        case 'piedrapiedra':
-        case 'papelpapel':
-        case 'tijerastijeras':
-            draw(usuarioOpcione, compuOpcione);
-            break;
-
-    }
-}
-
-function main(){
-    piedra_div.addEventListener('click', function(){
-        play('piedra');
-    })
-    papel_div.addEventListener('click', function(){
-        play('papel');
-    })
-    tijeras_div.addEventListener('click', function(){
-        play('tijeras');
-    })
-}
-
-function clearModal(e){
-    if(e.target == modal) {
-        modal.style.display = "none"
-    }
-}
-
-function restartGame(){
-    usuarioScore = 0;
-    compuScore = 0;
-    usuarioScore_span.innerHTML = usuarioScore;
-    compuScore_span.innerHTML = compuScore;
-}
-
-restart.addEventListener('click', restartGame);
-window.addEventListener('click', clearModal);
-main ();
-
-
